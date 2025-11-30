f53cd7a705266fd9c0b5dd2e311ec2f8abc44ce2
YESSIN ht <Yessineht@gmail.com>
2025-12-01 00:42:32 +0100

    4th commit

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-Python 3.13.9 (tags/v3.13.9:8183fa5, Oct 14 2025, 14:09:13) [MSC v.1944 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
->>> from fastapi import FastAPI
-... import joblib
-... import pandas as pd
-... 
-... app = FastAPI()
-... 
-... model = joblib.load("gradianBoostingRegression.pkl")
-... 
-... @app.post("/predict")
-... def predict(data: dict):
-...     df = pd.DataFrame([data])
-...     pred = model.predict(df)[0]
+from fastapi import FastAPI
+import joblib
+import pandas as pd
+
+app = FastAPI()
+
+model = joblib.load("gradianBoostingRegression.pkl")
+
+@app.post("/predict")
+def predict(data: dict):
+    df = pd.DataFrame([data])
+    pred = model.predict(df)[0]
+    return {"prediction": float(pred)}
